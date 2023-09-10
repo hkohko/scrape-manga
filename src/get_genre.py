7db7b83a4c35afedfeed37028cceae6c3fd2e689
heko <91366057+hkohko@users.noreply.github.com>
2023-09-10 09:29:21 +0700

    scrapes and stores genre data (1 worker)

--- src/get_genre.py
+++ src/get_genre.py
@@ -2,7 +2,7 @@ import sqlite3
 import aiosqlite
 import asyncio
 from src.db.schema import insert_data_genre
-from src.main import get_response, get_ranges
+from src.main import get_response
 from src._logger import Logger
 from src._locations import Directories
 from bs4 import BeautifulSoup
@@ -13,7 +13,7 @@ Logger().basic_logger
 
 
 async def parse_html_genre(m_code: int) -> list[str]:
-    response = await get_response(m_code)
+    response, url = await get_response(m_code)
     soup = BeautifulSoup(response.text, "lxml")
     try:
         main_tree = soup.find("main")
@@ -29,15 +29,16 @@ async def genre_scraper(idx_lower: int, idx_upper: int):
     data = []
     async with aiosqlite.connect(DB) as conn:
         async with conn.execute(
-            "SELECT url_int FROM main WHERE (url_int BETWEEN ? AND ?)",
+            "SELECT title, url_int FROM main WHERE (url_int BETWEEN ? AND ?)",
             (idx_lower, idx_upper),
         ) as cursor:
-            async for idx in cursor:
-                genres = await parse_html_genre(idx[0])
+            async for title, idx in cursor:
+                genres = await parse_html_genre(idx)
                 if genres is not None:
-                    data.append({"genre": " ".join(genres)})
-                    print(data)
-                    insert_data_genre(data)  # inserts to a sqlite3 database
+                    data.append(
+                        {"title": title, "url_int": idx, "genre": " ".join(genres)}
+                    )
+                    await insert_data_genre(data)  # inserts to a sqlite3 database
                     data.clear()
 
 
@@ -47,11 +48,12 @@ async def genre(to_scrape: str):
     yes = ("y", "Y")
     auto_range = True if input("Automatically set ranges (y/n)? ") in yes else False
     max_id = next(id[0] for id in cursor.execute("SELECT MAX(url_int) FROM main"))
-    min_id = next(id[0] for id in cursor.execute("SELECT MIN(url_int) FROM main"))
+    min_id = next(id[0] for id in cursor.execute("SELECT MAX(url_int) FROM genre"))
     if auto_range:
         await genre_scraper(min_id, max_id)
     else:
         while True:
+            print(min_id)
             upper = int(input("upper limit: "))
             cursor.execute("SELECT * FROM main WHERE url_int=?", (upper,))
             upper_lim = [idx for idx in cursor]
